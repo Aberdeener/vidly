44206a2e4c9c52e62295af86c41842b92be5826e
Tadhg <tadhgsmboyle@gmail.com>
2020-03-27 22:43:08 -0700

    add customer endpoint

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const genres = require('./routes/genres')
+const customers = require('./routes/customers')
 const home = require('./routes/home')
 const port = process.env.PORT || 3000;
 
 app.use('/api/genres', genres)
+app.use('/api/customers', customers)
 app.use('/', home)
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
